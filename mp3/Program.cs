d80eecbcaeab892254a194a47be315d7fc37fe00
ryu <dragonash2200@gmail.com>
2016-02-22 01:51:29 +0900

    mp3

---
+++ mp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Shell32;
+
+class ShowID3Tags
+{
+    static void Main()
+    {
+
+        string dir = @"C:\Users\ryu\Desktop"; // MP3ファイルのあるディレクトリ
+        string file = "1-16 I Will.mp3";
+
+        ShellClass shell = new ShellClass();
+        Folder f = shell.NameSpace(dir);
+        FolderItem item = f.ParseName(file);
+        for (int i = 0; i < 200; i++)
+        {
+            if (!string.IsNullOrEmpty(f.GetDetailsOf(item, i))){
+                 Console.WriteLine(f.GetDetailsOf(item, i)); // アーティスト
+            }
+           
+        }
+    }
+}
